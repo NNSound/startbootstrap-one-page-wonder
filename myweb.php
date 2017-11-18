ad02468d9c05e46b8d1328c7d92bd55a07b52903
EC2 Default User <ec2-user@ip-172-31-31-237.us-west-2.compute.internal>
2017-11-18 16:55:22 +0000

    testing php

---
+++ myweb.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <!– 網頁使用的編碼 -->
+    <meta charset="utf-8">
+    <!– 控制網頁顯示依據裝置大小及倍率 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!–  設置 IE 瀏覽器的解析方式  -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>版面配置</title>
+    <link href="css/music.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <div>
+                <h1>NNcode</h1>
+            </div>
+        </header>
+        <div class="sidebar_left">
+            <nav>
+                <ul class="card">
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="tip.html">Tip</a>
+                    </li>
+                    <li>
+                        <a href="login.html">login</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="content">
+            <div class="table">
+                <table>
+                    
+                    <tr>
+                        <th style="border: 0;">#</th>
+                        <th>Song</th>
+                        <th>artist</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Despacito </td>
+                        <td>Luis Fonsi</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>See You Again</td>
+                        <td>Wiz Khalifa</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Sorry</td>
+                        <td>Justin Bieber</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Uptown Funk</td>
+                        <td>Mark Ronson</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Shape of You</td>
+                        <td>Ed Sheeran</td>
+                    </tr>
+
+                </table>
+            </div>
+            <div class="maketable">
+
+            </div>
+
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>
